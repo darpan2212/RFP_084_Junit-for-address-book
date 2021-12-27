44cd73a6253c236523c6a9a2b03bb505742c98f0
8948036201 <azharuddin.ansari108@gmail.com>
2021-12-27 18:49:55 +0530

    Password using Custom Exception

---
+++ src/com/bridgelabz/user_registration_problem_and_custom_exception/PasswordException.java
@@ -0,0 +1,31 @@
+package com.bridgelabz.user_registration_problem_and_custom_exception;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PasswordException {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the contact number : ");
+
+		String namePattern = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[@$!%*#?&])[A-Za-z0-9@$!%*#?&]{8,}$";
+		Pattern regex = Pattern.compile(namePattern);
+		String inputStr = sc.nextLine();
+		Matcher contactNumMatcher = regex.matcher(inputStr);
+		try {
+			int val = Integer.parseInt("Priyanka");
+			sc.close();
+			System.out.print(val);
+
+			if (contactNumMatcher.matches()) {
+				System.out.println(inputStr + "\nis valid password.");
+			} else {
+				System.out.println(inputStr + "\nis invalid password.Kindly input the right one.");
+			}
+		} catch (NumberFormatException e) {
+			System.out.println("Number format exception");
+		}
+	}
+}
